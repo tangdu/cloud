ec15a7ac57188a46eda479c01512016a500eda9f
唐杜 <tangdu@qccr.com>
2017-01-15 21:45:31 +0800

    增加HyMonitor

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 #SpringCloud Demo
 
-    ek-server   EK服务器
+    ek-server   EK服务器、HyMonitor
     user-facade API服务接口
     provider    API服务提供者
     consumer    API服务消费者
@@ -12,5 +12,8 @@
     http://localhost:9091/
 ##2.测试服务调用
     http://localhost:9092/s_hello?name=222
-##3.hystrix 
+##3.hystrix minito
+    http://localhost:9091/hystrix
+    输入：
     http://localhost:9092/hystrix.stream
+    点击监测即可
--- ek-server/src/main/java/com/tdu/run/ServerApplication.java
+++ ek-server/src/main/java/com/tdu/run/ServerApplication.java
@@ -3,9 +3,13 @@ package com.tdu.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServerApplication {
 
     public static void main(String[] args) {
